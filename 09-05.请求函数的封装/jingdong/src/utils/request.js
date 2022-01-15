825566a2a89f564bde125c7300f296bdb3e921ef
wangyongalive <jsonwang0102@163.com>
2022-01-15 17:09:28 +0800

    请求函数封装

--- 09-05.请求函数的封装/jingdong/src/utils/request.js
+++ 09-05.请求函数的封装/jingdong/src/utils/request.js
@@ -1,16 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
 
+/*封装请求函数*/
 export const post = (url, data = {}) => {
+  /*返回一个promise对象*/
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
-      baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => {
-      resolve(response.data)
-    }, err => {
-      reject(err)
-    })
-  })
-}
+    axios
+      .post(url, data, {
+        baseURL:
+          "https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(
+        (response) => {
+          /*直接获取到data数据*/
+          resolve(response.data);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+  });
+};
--- 09-05.请求函数的封装/jingdong/src/views/login/Login.vue
+++ 09-05.请求函数的封装/jingdong/src/views/login/Login.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="wrapper">
-    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png"/>
+    <img
+      class="wrapper__img"
+      src="http://www.dell-lee.com/imgs/vue3/user.png"
+    />
     <div class="wrapper__input">
       <input
         class="wrapper__input__content"
@@ -22,44 +25,48 @@
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
-import { post } from '../../utils/request'
-import { reactive } from 'vue'
+import { useRouter } from "vue-router";
+import { post } from "../../utils/request";
+import { reactive } from "vue";
 
 export default {
-  name: 'Login',
+  name: "Login",
   setup() {
     const data = reactive({
-      username: '',
-      password: ''
-    })
-    const router = useRouter()
+      username: "",
+      password: "",
+    });
+    const router = useRouter();
     const handleLogin = async () => {
       try {
-        const result = await post('/api/user/login', {
+        /*使用await来替代then*/
+        /*await报错之后就不会继续往下运行了*/
+        /*使用try catch来捕获失败*/
+        const result = await post("/api/user/login", {
           username: data.username,
-          password: data.password
-        })
+          password: data.password,
+        });
+        /*可选链*/
         if (result?.errno === 0) {
-          localStorage.isLogin = true
-          router.push({ name: 'Home' })
+          localStorage.isLogin = true;
+          router.push({ name: "Home" });
         } else {
-          alert('登陆失败')
+          alert("登陆失败");
         }
       } catch (e) {
-        alert('请求失败')
+        alert("请求失败");
       }
-    }
+    };
     const handleRegisterClick = () => {
-      router.push({ name: 'Register' })
-    }
-    return { handleLogin, handleRegisterClick, data }
-  }
-}
+      router.push({ name: "Register" });
+    };
+    return { handleLogin, handleRegisterClick, data };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
+@import "../../style/viriables.scss";
 .wrapper {
   position: absolute;
   top: 50%;
@@ -68,25 +75,25 @@ export default {
   transform: translateY(-50%);
   &__img {
     display: block;
-    margin: 0 auto .4rem auto;
-    width: .66rem;
-    height: .66rem;
+    margin: 0 auto 0.4rem auto;
+    width: 0.66rem;
+    height: 0.66rem;
   }
   &__input {
-    height: .48rem;
-    margin: 0 .4rem .16rem .4rem;
-    padding: 0 .16rem;
-    background: #F9F9F9;
-    border: 1px solid rgba(0,0,0,0.10);
+    height: 0.48rem;
+    margin: 0 0.4rem 0.16rem 0.4rem;
+    padding: 0 0.16rem;
+    background: #f9f9f9;
+    border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 6px;
     border-radius: 6px;
     &__content {
-      line-height: .48rem;
+      line-height: 0.48rem;
       border: none;
       outline: none;
       width: 100%;
       background: none;
-      font-size: .16rem;
+      font-size: 0.16rem;
       color: $content-notice-fontcolor;
       &::placeholder {
         color: $content-notice-fontcolor;
@@ -94,19 +101,19 @@ export default {
     }
   }
   &__login-button {
-    margin: .32rem .4rem .16rem .4rem;
-    line-height: .48rem;
-    background: #0091FF;
-    box-shadow: 0 .04rem .08rem 0 rgba(0,145,255,0.32);
-    border-radius: .04rem;
-    border-radius: .04rem;
+    margin: 0.32rem 0.4rem 0.16rem 0.4rem;
+    line-height: 0.48rem;
+    background: #0091ff;
+    box-shadow: 0 0.04rem 0.08rem 0 rgba(0, 145, 255, 0.32);
+    border-radius: 0.04rem;
+    border-radius: 0.04rem;
     color: #fff;
-    font-size: .16rem;
+    font-size: 0.16rem;
     text-align: center;
   }
   &__login-link {
     text-align: center;
-    font-size: .14rem;
+    font-size: 0.14rem;
     color: $content-notice-fontcolor;
   }
 }
