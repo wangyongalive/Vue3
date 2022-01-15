07a8eb1850f4c03232752c2a23833fb6e1c063d8
wangyongalive <jsonwang0102@163.com>
2022-01-15 22:53:07 +0800

    首页附近店铺数据动态化

--- 10-01.首页附近店铺数据动态化/jingdong/src/utils/request.js
+++ 10-01.首页附近店铺数据动态化/jingdong/src/utils/request.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+/*创建一个instance的实例*/
 const instance = axios.create({
   baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
   timeout: 10000
 })
 
+/*get方法 params*/
 export const get = (url, params = {}) => {
   return new Promise((resolve, reject) => {
     instance.get(url, { params }).then((response) => {
@@ -15,6 +17,7 @@ export const get = (url, params = {}) => {
   })
 }
 
+/*post方法*/
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
     instance.post(url, data, {
--- 10-01.首页附近店铺数据动态化/jingdong/src/views/home/Nearby.vue
+++ 10-01.首页附近店铺数据动态化/jingdong/src/views/home/Nearby.vue
@@ -1,91 +1,92 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div
-      class="nearby__item"
-      v-for="item in nearbyList"
-      :key="item._id"
-    >
-      <img :src="item.imgUrl" class="nearby__item__img">
+    <div class="nearby__item" v-for="item in nearbyList" :key="item._id">
+      <img :src="item.imgUrl" class="nearby__item__img" />
       <div class="nearby__content">
-        <div class="nearby__content__title">{{item.name}}</div>
+        <div class="nearby__content__title">{{ item.name }}</div>
         <div class="nearby__content__tags">
-          <span class="nearby__content__tag">月售: {{item.sales}}</span>
-          <span class="nearby__content__tag">起送: {{item.expressLimit}}</span>
-          <span class="nearby__content__tag">基础运费: {{item.expressPrice}}</span>
+          <span class="nearby__content__tag">月售: {{ item.sales }}</span>
+          <span class="nearby__content__tag"
+            >起送: {{ item.expressLimit }}</span
+          >
+          <span class="nearby__content__tag"
+            >基础运费: {{ item.expressPrice }}</span
+          >
         </div>
-        <p class="nearby__content__highlight">{{item.slogan}}</p>
+        <p class="nearby__content__highlight">{{ item.slogan }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { get } from '../../utils/request'
+import { ref } from "vue";
+import { get } from "../../utils/request";
 
+/*附近商店逻辑*/
 const useNearbyListEffect = () => {
-  const nearbyList = ref([]);
+  const nearbyList = ref([]); /*响应式的数据*/
   const getNearbyList = async () => {
-    const result = await get('/api/shop/hot-list')
+    const result = await get("/api/shop/hot-list");
     if (result?.errno === 0 && result?.data?.length) {
-      nearbyList.value = result.data
+      nearbyList.value = result.data;
     }
-  }
-  return { nearbyList, getNearbyList}
-}
+  };
+  return { nearbyList, getNearbyList };
+};
 
 export default {
-  name: 'Nearby',
+  name: "Nearby",
   setup() {
     const { nearbyList, getNearbyList } = useNearbyListEffect();
     getNearbyList();
     return { nearbyList };
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
+@import "../../style/viriables.scss";
 .nearby {
   &__title {
-    margin: .16rem 0 .02rem 0;
-    font-size: .18rem;
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
     font-weight: normal;
     color: $content-fontcolor;
   }
   &__item {
     display: flex;
-    padding-top: .12rem;
+    padding-top: 0.12rem;
     &__img {
-      margin-right: .16rem;
-      width: .56rem;
-      height: .56rem;
+      margin-right: 0.16rem;
+      width: 0.56rem;
+      height: 0.56rem;
     }
   }
   &__content {
     flex: 1;
-    padding-bottom: .12rem;
+    padding-bottom: 0.12rem;
     border-bottom: 1px solid $content-bgColor;
     &__title {
-      line-height: .22rem;
-      font-size: .16rem;
+      line-height: 0.22rem;
+      font-size: 0.16rem;
       color: $content-fontcolor;
     }
     &__tags {
-      margin-top: .08rem;
-      line-height: .18rem;
-      font-size: .13rem;
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
       color: $content-fontcolor;
     }
     &__tag {
-      margin-right: .16rem;
+      margin-right: 0.16rem;
     }
     &__highlight {
-      margin: .08rem 0 0 0;
-      line-height: .18rem;
-      font-size: .13rem;
-      color: #E93B3B;
+      margin: 0.08rem 0 0 0;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: #e93b3b;
     }
   }
 }
