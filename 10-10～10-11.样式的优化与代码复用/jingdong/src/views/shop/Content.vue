6d727c759438c4d09faa685d211f1f86b6592d4c
wangyongalive <jsonwang0102@163.com>
2022-01-16 16:47:17 +0800

    样式的优化与代码复用

--- 10-10～10-11.样式的优化与代码复用/jingdong/src/views/shop/Content.vue
+++ 10-10～10-11.样式的优化与代码复用/jingdong/src/views/shop/Content.vue
@@ -10,7 +10,10 @@
     </div>
     <div class="product">
       <div class="product__item">
-        <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <img
+          class="product__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
         <div class="product__item__detail">
           <h4 class="product__item__title">番茄250g/份</h4>
           <p class="product__item__sales">月售10件</p>
@@ -30,29 +33,27 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
-@import '../../style/mixins.scss';
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
 .content {
   display: flex;
   position: absolute;
   left: 0;
   right: 0;
   top: 1.5rem;
-  bottom: .5rem;
+  bottom: 0.5rem;
 }
 .category {
   overflow-y: scroll;
   height: 100%;
-  width: .76rem;
+  width: 0.76rem;
   background: $search-bgColor;
   &__item {
-    line-height: .4rem;
+    line-height: 0.4rem;
     text-align: center;
     font-size: 14px;
     color: #333;
@@ -67,71 +68,71 @@ export default {
   &__item {
     position: relative;
     display: flex;
-    padding: .12rem 0;
-    margin: 0 .16rem;
-    border-bottom: .01rem solid $content-bgColor;
+    padding: 0.12rem 0;
+    margin: 0 0.16rem;
+    border-bottom: 0.01rem solid $content-bgColor;
     &__detail {
-      overflow: hidden;
+      overflow: hidden; /*超出隐藏 而不是显示滚动条*/ /*默认值。内容不会被修剪，可以呈现在元素框之外。*/
     }
     &__img {
-      width: .68rem;
-      height: .68rem;
-      margin-right: .16rem;
+      width: 0.68rem;
+      height: 0.68rem;
+      margin-right: 0.16rem;
     }
     &__title {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: $content-fontcolor;
-      @include ellipsis;
+      @include ellipsis;  /*超出规定字符显示...*/
     }
     &__sales {
-      margin: .06rem 0;
-      font-size: .12rem;
+      margin: 0.06rem 0;
+      font-size: 0.12rem;
       color: $content-fontcolor;
     }
     &__price {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: $hightlight-fontColor;
     }
     &__yen {
-      font-size: .12rem;
+      font-size: 0.12rem;
     }
     &__origin {
-      margin-left: .06rem;
-      line-height: .2rem;
-      font-size: .12rem;
+      margin-left: 0.06rem;
+      line-height: 0.2rem;
+      font-size: 0.12rem;
       color: $light-fontColor;
       text-decoration: line-through;
     }
     .product__number {
-      position: absolute;
+      position: absolute; /*定位*/
       right: 0;
-      bottom: .12rem;
-      &__minus, &__plus
-       {
-        display: inline-block;
-        width: .2rem;
-        height: .2rem;
-        line-height: .16rem;;
+      bottom: 0.12rem;
+      &__minus,
+      &__plus {
+        /*设置加号和减号的样式*/
+        display: inline-block; /*inline-block才能设置宽高*/
+        width: 0.2rem;
+        height: 0.2rem;
+        line-height: 0.16rem;
         border-radius: 50%;
-        font-size: .2rem;
+        font-size: 0.2rem;
         text-align: center;
       }
       &__minus {
-        border: .01rem solid $medium-fontColor;
+        border: 0.01rem solid $medium-fontColor;
         color: $medium-fontColor;
-        margin-right: .05rem;
+        margin-right: 0.05rem; /*调整左右间距*/
       }
       &__plus {
         background: $btn-bgColor;
-        color: $bgColor;
-        margin-left: .05rem;
+        color: $bgColor; /*抽离样式代码  颜色复用*/
+        margin-left: 0.05rem; /*调整左右间距*/
       }
     }
   }
-  
 }
 </style>
