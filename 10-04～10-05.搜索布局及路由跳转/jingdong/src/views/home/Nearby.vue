f9629d6bb10096c7076d0f60536a9c0cbd79f463
wangyongalive <jsonwang0102@163.com>
2022-01-16 14:27:25 +0800

    搜索布局及路由跳转

--- 10-04～10-05.搜索布局及路由跳转/jingdong/src/views/home/Nearby.vue
+++ 10-04～10-05.搜索布局及路由跳转/jingdong/src/views/home/Nearby.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
+    <!-- 循环router-link -->
     <router-link to="/shop" v-for="item in nearbyList" :key="item._id">
       <ShopInfo :item="item" />
     </router-link>
@@ -8,43 +9,43 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import { get } from '../../utils/request'
-import ShopInfo from '../../components/ShopInfo'
+import { ref } from "vue";
+import { get } from "../../utils/request";
+import ShopInfo from "../../components/ShopInfo";
 
 const useNearbyListEffect = () => {
   const nearbyList = ref([]);
   const getNearbyList = async () => {
-    const result = await get('/api/shop/hot-list')
+    const result = await get("/api/shop/hot-list");
     if (result?.errno === 0 && result?.data?.length) {
-      nearbyList.value = result.data
+      nearbyList.value = result.data;
     }
-  }
-  return { nearbyList, getNearbyList}
-}
+  };
+  return { nearbyList, getNearbyList };
+};
 
 export default {
-  name: 'Nearby',
+  name: "Nearby",
   components: { ShopInfo },
   setup() {
     const { nearbyList, getNearbyList } = useNearbyListEffect();
     getNearbyList();
     return { nearbyList };
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
+@import "../../style/viriables.scss";
 .nearby {
   &__title {
-    margin: .16rem 0 .02rem 0;
-    font-size: .18rem;
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
     font-weight: normal;
     color: $content-fontcolor;
   }
   a {
-    text-decoration: none;
+    text-decoration: none; /*取消router-link的下划线*/
   }
 }
 </style>
