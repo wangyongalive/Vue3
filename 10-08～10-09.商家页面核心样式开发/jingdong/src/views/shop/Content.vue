ba744c525617d9107e80db662ceac8425e9ecba2
wangyongalive <jsonwang0102@163.com>
2022-01-16 16:06:01 +0800

    商家页面核心样式开发

--- 10-08～10-09.商家页面核心样式开发/jingdong/src/views/shop/Content.vue
+++ 10-08～10-09.商家页面核心样式开发/jingdong/src/views/shop/Content.vue
@@ -10,11 +10,15 @@
     </div>
     <div class="product">
       <div class="product__item">
-        <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <img
+          class="product__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
         <div class="product__item__detail">
           <h4 class="product__item__title">番茄250g/份</h4>
           <p class="product__item__sales">月售10件</p>
           <p class="product__item__price">
+            <!-- &yen; ￥ 字符实体 -->
             <span class="product__item__yen">&yen;</span>33.6
             <span class="product__item__origin">&yen;66.6</span>
           </p>
@@ -25,9 +29,7 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
@@ -37,20 +39,20 @@ export default {
   left: 0;
   right: 0;
   top: 1.5rem;
-  bottom: .5rem;
+  bottom: 0.5rem;
 }
 .category {
   overflow-y: scroll;
   height: 100%;
-  width: .76rem;
-  background: #F5F5F5;
+  width: 0.76rem;
+  background: #f5f5f5;
   &__item {
-    line-height: .4rem;
+    line-height: 0.4rem; /*设置行高 文字可以直接垂直居中*/
     text-align: center;
     font-size: 14px;
     color: #333;
     &--active {
-      background: #FFF;
+      background: #fff;
     }
   }
 }
@@ -59,40 +61,40 @@ export default {
   flex: 1;
   &__item {
     display: flex;
-    padding: .12rem 0;
-    margin: 0 .16rem;
-    border-bottom: .01rem solid #F1F1F1;
+    padding: 0.12rem 0;
+    margin: 0 0.16rem; /*border不会出现在margin上*/
+    border-bottom: 0.01rem solid #f1f1f1;
     &__img {
-      width: .68rem;
-      height: .68rem;
-      margin-right: .16rem;
+      width: 0.68rem;
+      height: 0.68rem;
+      margin-right: 0.16rem;
     }
     &__title {
-      margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      margin: 0; /*取消默认的样式*/
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: #333;
     }
     &__sales {
-      margin: .06rem 0;
-      font-size: .12rem;
+      margin: 0.06rem 0;
+      font-size: 0.12rem;
       color: #333;
     }
     &__price {
-      margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
-      color: #E93B3B;
+      margin: 0; /*消除p标签默认样式*/
+      line-height: 0.2rem;
+      font-size: 0.14rem;
+      color: #e93b3b;
     }
     &__yen {
-      font-size: .12rem;
+      font-size: 0.12rem;
     }
     &__origin {
-      margin-left: .06rem;
-      line-height: .2rem;
-      font-size: .12rem;
+      margin-left: 0.06rem;
+      line-height: 0.2rem;
+      font-size: 0.12rem;
       color: #999;
-      text-decoration: line-through;
+      text-decoration: line-through; /*设置文本的修饰线外观*/
     }
   }
 }
