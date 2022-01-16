e9197a9f759989fc0f47e9e4578cbe2bc39f5b0f
wangyongalive <jsonwang0102@163.com>
2022-01-16 14:56:50 +0800

    路由参数的传递以及商家详情的获取

--- 10-06～10-07.路由参数的传递以及商家详情的获取/jingdong/src/router/index.js
+++ 10-06～10-07.路由参数的传递以及商家详情的获取/jingdong/src/router/index.js
@@ -1,29 +1,38 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/home/Home')
-  }, {
-    path: '/shop/:id',
-    name: 'Shop',
-    component: () => import(/* webpackChunkName: "shop" */ '../views/shop/Shop')
-  }, {
-    path: '/register',
-    name: 'Register',
-    component: () => import(/* webpackChunkName: "register" */ '../views/register/Register'),
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/home/Home"),
+  },
+  {
+    /*路由参数的传递*/
+    path: "/shop/:id",
+    name: "Shop",
+    component: () =>
+      import(/* webpackChunkName: "shop" */ "../views/shop/Shop"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "../views/register/Register"),
     beforeEnter(to, from, next) {
       const { isLogin } = localStorage;
-      isLogin ? next({ name: 'Home'}):  next();
-    }
-  }, {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/login/Login'),
+      isLogin ? next({ name: "Home" }) : next();
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/login/Login"),
     beforeEnter(to, from, next) {
       const { isLogin } = localStorage;
-      isLogin ? next({ name: 'Home'}):  next();
-    }
+      isLogin ? next({ name: "Home" }) : next();
+    },
   },
   // {
   //   path: '/about',
@@ -31,20 +40,20 @@ const routes = [{
   //   // route level code-splitting
   //   // this generates a separate chunk (about.[hash].js) for this route
   //   // which is lazy-loaded when the route is visited.
-  //   
+  //
   // }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-router.beforeEach((to, from ,next) => {
+router.beforeEach((to, from, next) => {
   const { isLogin } = localStorage;
   const { name } = to;
-  const isLoginOrRegister = (name === "Login" || name === "Register");
-  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login'});
-})
+  const isLoginOrRegister = name === "Login" || name === "Register";
+  isLogin || isLoginOrRegister ? next() : next({ name: "Login" });
+});
 
-export default router
+export default router;
--- 10-06～10-07.路由参数的传递以及商家详情的获取/jingdong/src/views/shop/Shop.vue
+++ 10-06～10-07.路由参数的传递以及商家详情的获取/jingdong/src/views/shop/Shop.vue
@@ -1,98 +1,94 @@
 <template>
   <div class="wrapper">
     <div class="search">
-      <div class="search__back iconfont" @click="handleBackClick">
-        &#xe6f2;
-      </div>
+      <div class="search__back iconfont" @click="handleBackClick">&#xe6f2;</div>
       <div class="search__content">
         <span class="search__content__icon iconfont">&#xe62d;</span>
-        <input
-          class="search__content__input"
-          placeholder="请输入商品名称"
-        />
+        <input class="search__content__input" placeholder="请输入商品名称" />
       </div>
     </div>
-    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl"/>
+    <!-- 图片为获取到时候的问题 -->
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl" />
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-import { get } from '../../utils/request'
-import ShopInfo from '../../components/ShopInfo'
+import { reactive, toRefs } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { get } from "../../utils/request";
+import ShopInfo from "../../components/ShopInfo";
 
 // 获取当前商铺信息
 const useShopInfoEffect = () => {
-  const route = useRoute()
-  const data = reactive({ item: {} })
+  const route = useRoute();
+  const data = reactive({ item: {} }); /*定义一个具有item属性的响应式对象*/
   const getItemData = async () => {
-    const result = await get(`/api/shop/${route.params.id}`)
-    if(result?.errno === 0 && result?.data) {
-      data.item = result.data
+    const result = await get(`/api/shop/${route.params.id}`);
+    if (result?.errno === 0 && result?.data) {
+      data.item = result.data;
     }
-  }
-  const { item } = toRefs(data)
-  return { item, getItemData }
-}
+  };
+  const { item } = toRefs(data);
+  return { item, getItemData };
+};
 
 // 点击回退逻辑
 const useBackRouterEffect = () => {
-  const router = useRouter()
+  const router = useRouter();
   const handleBackClick = () => {
-    router.back()
-  }
-  return handleBackClick
-}
+    router.back();
+  };
+  return handleBackClick;
+};
 
 export default {
-  name: 'Shop',
+  name: "Shop",
   components: { ShopInfo },
   setup() {
-    const { item, getItemData } = useShopInfoEffect()
-    const handleBackClick = useBackRouterEffect()
-    getItemData()
-    return { item, handleBackClick }
-  }
-}
+    const { item, getItemData } = useShopInfoEffect();
+    const handleBackClick = useBackRouterEffect();
+    getItemData();
+    return { item, handleBackClick };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
+@import "../../style/viriables.scss";
 .wrapper {
-  padding: 0 .18rem;
+  padding: 0 0.18rem;
 }
 .search {
   display: flex;
-  margin: .14rem 0 .04rem 0;
-  line-height: .32rem;
+  margin: 0.14rem 0 0.04rem 0; /*修改上下距离的样式*/
+  line-height: 0.32rem;
   &__back {
-    width: .3rem;
-    font-size: .24rem;
-    color: #B6B6B6;
+    width: 0.3rem;
+    font-size: 0.24rem;
+    color: #b6b6b6;
   }
   &__content {
     display: flex;
     flex: 1;
-    background: $search-bgColor;
-    border-radius: .16rem;
+    background: $search-bgColor; /*样式抽离为变量*/
+    border-radius: 0.16rem;
     &__icon {
-      width: .44rem;
+      width: 0.44rem;
       text-align: center;
-      color: $search-fontColor;
+      color: $search-fontColor; /*样式抽离为变量*/
     }
     &__input {
       display: block;
       width: 100%;
-      padding-right: .2rem;
+      padding-right: 0.2rem;
       border: none;
       outline: none;
       background: none;
-      height: .32rem;
-      font-size: .14rem;
-      color: $content-fontcolor;
+      height: 0.32rem;
+      font-size: 0.14rem;
+      color: $content-fontcolor; /*样式抽离为变量*/
       &::placeholder {
-        color: $content-fontcolor;
+        color: $content-fontcolor; /*样式抽离为变量*/
       }
     }
   }
