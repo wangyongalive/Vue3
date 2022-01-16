a5bad932255336bfcc39dd46da7cd308122b5f82
wangyongalive <jsonwang0102@163.com>
2022-01-16 17:18:41 +0800

    首页详情页分类及内容联动的实现

--- 10-12.商家详情页分类及内容联动的实现/jingdong/src/views/shop/Content.vue
+++ 10-12.商家详情页分类及内容联动的实现/jingdong/src/views/shop/Content.vue
@@ -2,27 +2,30 @@
   <div class="content">
     <div class="category">
       <div
-        :class="{'category__item': true, 'category__item--active': currentTab === item.tab}"
+        :class="{
+          category__item: true,
+          'category__item--active': currentTab === item.tab,
+        }"
         v-for="item in categories"
         :key="item.name"
-        @click="() => handleCategoryClick(item.tab)"
+        @click="handleCategoryClick(item.tab)"
       >
-        {{item.name}}
+        <!-- @click="() => handleCategoryClick(item.tab)" -->
+        {{ item.name }}
       </div>
     </div>
     <div class="product">
-      <div
-        class="product__item"
-        v-for="item in contentList"
-        :key="item._id"
-      >
-        <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+      <div class="product__item" v-for="item in contentList" :key="item._id">
+        <img
+          class="product__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
         <div class="product__item__detail">
-          <h4 class="product__item__title">{{item.name}}</h4>
-          <p class="product__item__sales">月售 {{item.sales}} 件</p>
+          <h4 class="product__item__title">{{ item.name }}</h4>
+          <p class="product__item__sales">月售 {{ item.sales }} 件</p>
           <p class="product__item__price">
-            <span class="product__item__yen">&yen;</span>{{item.price}}
-            <span class="product__item__origin">&yen;{{item.oldPrice}}</span>
+            <span class="product__item__yen">&yen;</span>{{ item.price }}
+            <span class="product__item__origin">&yen;{{ item.oldPrice }}</span>
           </p>
         </div>
         <div class="product__number">
@@ -36,60 +39,65 @@
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
-import { get } from '../../utils/request'
+import { reactive, toRefs } from "vue";
+import { get } from "../../utils/request";
 export default {
-  name: 'Content',
+  name: "Content",
   setup() {
-    const categories = [{
-      name: '全部商品',
-      tab: 'all'
-    }, {
-      name:'秒杀',
-      tab: 'seckill'
-    }, {
-      name: '新鲜水果',
-      tab: 'fruit'
-    }]
+    const categories = [
+      {
+        name: "全部商品",
+        tab: "all",
+      },
+      {
+        name: "秒杀",
+        tab: "seckill",
+      },
+      {
+        name: "新鲜水果",
+        tab: "fruit",
+      },
+    ];
+    /*定义响应式对象*/
     const data = reactive({
       currentTab: categories[0].tab,
-      contentList:[]
-    })
+      contentList: [],
+    });
     const getContentData = async (tab) => {
-      const result = await get('/api/shop/1/products', {tab})
-      if(result?.errno === 0 && result?.data?.length) {
+      const result = await get("/api/shop/1/products", { tab });
+      if (result?.errno === 0 && result?.data?.length) {
         data.contentList = result.data;
       }
-    }
-    const handleCategoryClick = (tab) => {
-      getContentData(tab)
-      data.currentTab = tab
-    }
-    getContentData('all')
-    const { contentList, currentTab } = toRefs(data)
-      return { categories, currentTab, contentList, handleCategoryClick}
-    }
-}
+    };
+    const handleCategoryClick = (tab) => { 
+      getContentData(tab);
+      data.currentTab = tab;
+    };
+    getContentData("all");
+    const { contentList, currentTab } = toRefs(data); /*解构响应式对象*/
+    return { categories, currentTab, contentList, handleCategoryClick };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
-@import '../../style/mixins.scss';
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
 .content {
   display: flex;
   position: absolute;
   left: 0;
   right: 0;
   top: 1.5rem;
-  bottom: .5rem;
+  bottom: 0.5rem;
 }
 .category {
   overflow-y: scroll;
   height: 100%;
-  width: .76rem;
+  width: 0.76rem;
   background: $search-bgColor;
   &__item {
-    line-height: .4rem;
+    line-height: 0.4rem;
     text-align: center;
     font-size: 14px;
     color: #333;
@@ -104,71 +112,70 @@ export default {
   &__item {
     position: relative;
     display: flex;
-    padding: .12rem 0;
-    margin: 0 .16rem;
-    border-bottom: .01rem solid $content-bgColor;
+    padding: 0.12rem 0;
+    margin: 0 0.16rem;
+    border-bottom: 0.01rem solid $content-bgColor;
     &__detail {
       overflow: hidden;
     }
     &__img {
-      width: .68rem;
-      height: .68rem;
-      margin-right: .16rem;
+      width: 0.68rem;
+      height: 0.68rem;
+      margin-right: 0.16rem;
     }
     &__title {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: $content-fontcolor;
       @include ellipsis;
     }
     &__sales {
-      margin: .06rem 0;
-      font-size: .12rem;
+      margin: 0.06rem 0;
+      font-size: 0.12rem;
       color: $content-fontcolor;
     }
     &__price {
       margin: 0;
-      line-height: .2rem;
-      font-size: .14rem;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
       color: $hightlight-fontColor;
     }
     &__yen {
-      font-size: .12rem;
+      font-size: 0.12rem;
     }
     &__origin {
-      margin-left: .06rem;
-      line-height: .2rem;
-      font-size: .12rem;
+      margin-left: 0.06rem;
+      line-height: 0.2rem;
+      font-size: 0.12rem;
       color: $light-fontColor;
       text-decoration: line-through;
     }
     .product__number {
       position: absolute;
       right: 0;
-      bottom: .12rem;
-      &__minus, &__plus
-       {
+      bottom: 0.12rem;
+      &__minus,
+      &__plus {
         display: inline-block;
-        width: .2rem;
-        height: .2rem;
-        line-height: .16rem;;
+        width: 0.2rem;
+        height: 0.2rem;
+        line-height: 0.16rem;
         border-radius: 50%;
-        font-size: .2rem;
+        font-size: 0.2rem;
         text-align: center;
       }
       &__minus {
-        border: .01rem solid $medium-fontColor;
+        border: 0.01rem solid $medium-fontColor;
         color: $medium-fontColor;
-        margin-right: .05rem;
+        margin-right: 0.05rem;
       }
       &__plus {
         background: $btn-bgColor;
         color: $bgColor;
-        margin-left: .05rem;
+        margin-left: 0.05rem;
       }
     }
   }
-  
 }
 </style>
