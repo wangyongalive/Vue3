6fc329d71259a27a1afc51150657a2d62a0fac35
wangyongalive <jsonwang0102@163.com>
2022-01-18 22:10:19 +0800

    '购物车数据结构的变更'

--- 11-13～11-14.购物车数据结构的变更/jingdong/src/store/index.js
+++ 11-13～11-14.购物车数据结构的变更/jingdong/src/store/index.js
@@ -1,53 +1,60 @@
-import Vuex from 'vuex'
+import Vuex from "vuex";
 
 export default Vuex.createStore({
   state: {
     // { shopId: {shopName:'', productList:{ productId: {} }}}
-    cartList: {}
+    cartList: {},
   },
   mutations: {
     changeCartItemInfo(state, payload) {
-      const { shopId, productId, productInfo } = payload
+      const { shopId, productId, productInfo } = payload;
       let shopInfo = state.cartList[shopId] || {
-        shopName: '', productList:{}
+        shopName: "",
+        productList: {},
+      };
+      let product = shopInfo.productList[productId];
+      if (!product) {
+        productInfo.count = 0;
+        product = productInfo;
       }
-      let product = shopInfo.productList[productId]
-      if(!product) {
-        productInfo.count = 0
-        product = productInfo
+      product.count = product.count + payload.num;
+      if (payload.num > 0) {
+        product.check = true;
       }
-      product.count = product.count + payload.num
-      if(payload.num > 0) { product.check = true }
-      if(product.count < 0) { product.count = 0 }
-      shopInfo.productList[productId] = product
-      state.cartList[shopId] = shopInfo
+      if (product.count < 0) {
+        product.count = 0;
+      }
+      shopInfo.productList[productId] = product;
+      state.cartList[shopId] = shopInfo;
     },
+    /*改变商铺的名称*/
     changeShopName(state, payload) {
-      const { shopId, shopName } = payload
+      const { shopId, shopName } = payload;
       const shopInfo = state.cartList[shopId] || {
-        shopName: '', productList:{}
-      }
-      shopInfo.shopName = shopName
-      state.cartList[shopId] = shopInfo
+        shopName: "",
+        productList: {},
+      };
+      shopInfo.shopName = shopName;
+      state.cartList[shopId] = shopInfo;
     },
     changeCartItemChecked(state, payload) {
-      const { shopId, productId } = payload
-      const product = state.cartList[shopId].productList[productId]
-      product.check = !product.check
+      const { shopId, productId } = payload;
+      const product = state.cartList[shopId].productList[productId];
+      product.check = !product.check;
     },
     cleanCartProducts(state, payload) {
-      const { shopId } = payload
-      state.cartList[shopId].productList = {}
+      const { shopId } = payload;
+      state.cartList[shopId].productList = {};
     },
     setCartItemsChecked(state, payload) {
-      const { shopId } = payload
-      const products = state.cartList[shopId].productList
-      if(products) {
-        for(let key in products) {
-          const product = products[key]
-          product.check = true
+      const { shopId } = payload;
+      const products = state.cartList[shopId].productList;
+      if (products) {
+        for (let key in products) {
+          const product = products[key];
+          product.check = true;
         }
       }
-    }
-  }
-})
+    },
+  },
+});
